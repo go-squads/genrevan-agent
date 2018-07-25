8eed35e9df7bfc3b45198cea11377902f418ab3e
Eko Simanjuntak <ecojuntak@gmail.com>
2018-07-25 04:22:43 +0000

    Create collector to collect metric

---
+++ collector/collector.go
@@ -0,0 +1,49 @@
+package collector
+
+import(
+  "github.com/shirou/gopsutil/cpu"
+  "github.com/shirou/gopsutil/mem"
+  "fmt"
+  "strconv"
+  "net/http"
+  "bytes"
+  "net/url"
+  "github.com/go-squads/genrevan-agent/config"
+)
+
+func getCPULoad() string {
+  cpuLoad, err := cpu.Percent(0, false)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  return strconv.FormatFloat(cpuLoad[0], 'f', 3, 64)
+}
+
+func getMemoryLoad() string {
+  memoryLoad, err := mem.VirtualMemory()
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  return fmt.Sprint(memoryLoad.Used / (1024*1024))
+}
+
+func SendCurrentLoad(conf *config.Conf) {
+  data := url.Values{}
+  data.Add("cpu", getCPULoad())
+  data.Add("memory", getMemoryLoad())
+
+  client := &http.Client{}
+  body := bytes.NewBufferString(data.Encode())
+  req, err := http.NewRequest(http.MethodPut, "http://"+conf.SchedulerIp+":"+conf.SchedulerPort+"/metric/"+strconv.Itoa(conf.LxdId), body)
+  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+  respond, err := client.Do(req)
+
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  fmt.Println(respond)
+}
